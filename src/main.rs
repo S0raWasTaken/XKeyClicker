a8673bca0bd9fe060dbb4824fe5a2b1f7d20a82a
S0raWasTaken <s0ra@duck.com>
2022-10-02 03:38:54 -0500

    feat: Add more cooldown options
    Also replace all statics with a struct

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@
 use std::{
     sync::{
         mpsc::{channel, Receiver, Sender},
-        Arc, Mutex,
+        Arc,
     },
     thread::{sleep, spawn},
-    time::Duration,
 };
 
 use gtk::{
@@ -16,46 +15,51 @@ use gtk::{
     traits::{ButtonExt, EntryExt, GtkWindowExt, WidgetExt},
     Application, ApplicationWindow, Builder, Button, EditableSignals, Entry,
 };
-use primitives::{KeyType, NotMut, SendBox};
-use rdev::{listen, simulate, Event, EventType, Key};
-
-static KEYBIND: Mutex<Key> = Mutex::new(Key::F7);
-static STATE: Mutex<bool> = Mutex::new(false);
-static SHOULD_RECV: Mutex<KeyType> = Mutex::new(KeyType::None);
-static COOLDOWN: Mutex<u64> = Mutex::new(100);
-static REPEATED_KEY: Mutex<Option<Key>> = Mutex::new(None);
+use primitives::{KeyType, NotMut, SendBox, XKeyClicker};
+use rdev::{listen, simulate, Event, EventType};
 
 mod primitives;
 
+type ArcXKeyClicker = Arc<XKeyClicker>;
+
 fn main() {
+    let xkeyclicker = XKeyClicker::new();
+
     let (entry_sender, entry_receiver) = channel();
     let entry_receiver = Arc::new(SendBox(entry_receiver));
 
+    let xkc_handle = xkeyclicker.clone();
     // Spawn keybind listener
-    spawn(move || listen(move |e| keybind(&e, &entry_receiver.clone())).unwrap());
+    spawn(move || {
+        listen(move |e| {
+            keybind(&e, &entry_receiver.clone(), &xkc_handle.clone());
+        })
+        .unwrap();
+    });
 
+    let xkc_handle = xkeyclicker.clone();
     // Spawn auto clicker
-    spawn(auto_clicker);
+    spawn(move || auto_clicker(&xkc_handle));
 
     let app = Application::new(Some("com.s0ra.xkeyclicker"), ApplicationFlags::default());
-    app.connect_activate(move |app| build_ui(app, entry_sender.clone()));
+    app.connect_activate(move |app| build_ui(app, entry_sender.clone(), xkeyclicker.clone()));
     app.run();
 }
 
-fn auto_clicker() {
+fn auto_clicker(xkc_handle: &ArcXKeyClicker) {
     loop {
-        if *STATE.lock().unwrap() {
-            if let Some(key) = *REPEATED_KEY.lock().unwrap() {
-                let delay = Duration::from_millis(*COOLDOWN.lock().unwrap());
+        if *xkc_handle.state.lock().unwrap() {
+            if let Some(key) = *xkc_handle.repeated_key.lock().unwrap() {
+                let delay = &*xkc_handle.cooldown.lock().unwrap();
                 simulate(&EventType::KeyPress(key)).unwrap();
                 simulate(&EventType::KeyRelease(key)).unwrap();
-                sleep(delay);
+                sleep(delay.as_duration());
             }
         }
     }
 }
 
-fn build_ui(app: &Application, entry_sender: Sender<Entry>) {
+fn build_ui(app: &Application, entry_sender: Sender<Entry>, xkc_handle: ArcXKeyClicker) {
     let builder = Builder::from_string(include_str!("xkeyclicker.ui"));
     let window: ApplicationWindow = builder.object("window").unwrap();
 
@@ -63,12 +67,38 @@ fn build_ui(app: &Application, entry_sender: Sender<Entry>) {
 
     let time_millis: Entry = builder.object("time_millis").unwrap();
 
-    time_millis.connect_changed(|entry| {
+    let xkc_handle_clone = xkc_handle.clone();
+
+    time_millis.connect_changed(move |entry| {
         if let Ok(cooldown) = entry.buffer().text().parse::<u64>() {
-            *COOLDOWN.lock().unwrap() = cooldown;
+            xkc_handle_clone.cooldown.lock().unwrap().millis = cooldown;
         } else if !entry.buffer().text().is_empty() {
             entry.set_text("100");
-            *COOLDOWN.lock().unwrap() = 100;
+            xkc_handle_clone.cooldown.lock().unwrap().millis = 100;
+        }
+    });
+
+    let time_micros: Entry = builder.object("time_micros").unwrap();
+    let xkc_handle_clone = xkc_handle.clone();
+
+    time_micros.connect_changed(move |entry| {
+        if let Ok(cooldown) = entry.buffer().text().parse::<u64>() {
+            xkc_handle_clone.cooldown.lock().unwrap().micros = cooldown;
+        } else if !entry.buffer().text().is_empty() {
+            entry.set_text("0");
+            xkc_handle_clone.cooldown.lock().unwrap().micros = 0;
+        }
+    });
+
+    let time_nanos: Entry = builder.object("time_nanos").unwrap();
+    let xkc_handle_clone = xkc_handle.clone();
+
+    time_nanos.connect_changed(move |entry| {
+        if let Ok(cooldown) = entry.buffer().text().parse::<u64>() {
+            xkc_handle_clone.cooldown.lock().unwrap().nanos = cooldown;
+        } else if !entry.buffer().text().is_empty() {
+            entry.set_text("0");
+            xkc_handle_clone.cooldown.lock().unwrap().nanos = 0;
         }
     });
 
@@ -76,54 +106,72 @@ fn build_ui(app: &Application, entry_sender: Sender<Entry>) {
     let keybind_entry: Entry = builder.object("keybind_entry").unwrap();
 
     let entry_sender_copy = entry_sender.clone();
+    let xkc_handle_clone = xkc_handle.clone();
 
-    start_keybind_button
-        .connect_clicked(move |_| set_start_keybind(&entry_sender.clone(), &keybind_entry));
+    start_keybind_button.connect_clicked(move |_| {
+        set_start_keybind(
+            &entry_sender.clone(),
+            &keybind_entry,
+            &xkc_handle_clone.clone(),
+        );
+    });
 
     let key_selector_button: Button = builder.object("key_selector").unwrap();
     let repeated_key_entry: Entry = builder.object("repeated_key_entry").unwrap();
 
     key_selector_button.connect_clicked(move |_| {
-        set_repeated_key(&entry_sender_copy.clone(), &repeated_key_entry);
+        set_repeated_key(
+            &entry_sender_copy.clone(),
+            &repeated_key_entry,
+            &xkc_handle.clone(),
+        );
     });
 
     window.show_all();
 }
 
-fn set_repeated_key(entry_sender: &Sender<Entry>, repeated_key_entry: &Entry) {
-    *SHOULD_RECV.lock().unwrap() = KeyType::Repeated;
+fn set_repeated_key(
+    entry_sender: &Sender<Entry>,
+    repeated_key_entry: &Entry,
+    xkc_handle: &ArcXKeyClicker,
+) {
+    *xkc_handle.should_recv.lock().unwrap() = KeyType::Repeated;
     repeated_key_entry.set_text("Press a key to bind");
     entry_sender.send(repeated_key_entry.clone()).unwrap();
 }
 
-fn set_start_keybind(entry_sender: &Sender<Entry>, keybind_entry: &Entry) {
-    *SHOULD_RECV.lock().unwrap() = KeyType::Keybind;
+fn set_start_keybind(
+    entry_sender: &Sender<Entry>,
+    keybind_entry: &Entry,
+    xkc_handle: &ArcXKeyClicker,
+) {
+    *xkc_handle.should_recv.lock().unwrap() = KeyType::Keybind;
     keybind_entry.set_text("Press a key to bind");
     entry_sender.send(keybind_entry.clone()).unwrap();
 }
 
-fn keybind(event: &Event, receiver: &Arc<SendBox<Receiver<Entry>>>) {
+fn keybind(event: &Event, receiver: &Arc<SendBox<Receiver<Entry>>>, xkc_handle: &ArcXKeyClicker) {
     if let Event {
         time: _,
         name: _,
         event_type: EventType::KeyPress(key),
     } = event
     {
-        let mut should_recv = SHOULD_RECV.lock().unwrap();
+        let mut should_recv = xkc_handle.should_recv.lock().unwrap();
         if let KeyType::Repeated = *should_recv {
-            *REPEATED_KEY.lock().unwrap() = Some(*key);
+            *xkc_handle.repeated_key.lock().unwrap() = Some(*key);
             *should_recv = KeyType::None;
 
             let entry = receiver.0.try_recv().unwrap();
             entry.set_text(&format!("{key:?}"));
         } else if let KeyType::Keybind = *should_recv {
-            *KEYBIND.lock().unwrap() = *key;
+            *xkc_handle.keybind.lock().unwrap() = *key;
             *should_recv = KeyType::None;
 
             let entry = receiver.0.try_recv().unwrap();
             entry.set_text(&format!("{key:?}"));
-        } else if *key == *KEYBIND.lock().unwrap() {
-            STATE.lock().unwrap().not_mut();
+        } else if *key == *xkc_handle.keybind.lock().unwrap() {
+            xkc_handle.state.lock().unwrap().not_mut();
         }
     }
 }
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,4 +1,10 @@
-use std::ops::Not;
+use std::{
+    ops::Not,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use rdev::Key;
 
 pub trait NotMut {
     fn not_mut(&mut self);
@@ -11,11 +17,64 @@ impl NotMut for bool {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub enum KeyType {
     Repeated,
     Keybind,
+    #[default]
     None,
+}
+
+#[derive(Debug)]
+pub struct XKeyClicker {
+    pub keybind: Mutex<Key>,
+    pub should_recv: Mutex<KeyType>,
+    pub state: Mutex<bool>,
+    pub cooldown: Mutex<Cooldown>,
+    pub repeated_key: Mutex<Option<Key>>,
+}
+
+impl Default for XKeyClicker {
+    fn default() -> Self {
+        Self {
+            keybind: Mutex::new(Key::F7),
+            should_recv: Mutex::default(),
+            state: Mutex::default(),
+            cooldown: Mutex::default(),
+            repeated_key: Mutex::default(),
+        }
+    }
+}
+
+impl XKeyClicker {
+    pub fn new() -> Arc<XKeyClicker> {
+        Arc::default()
+    }
+}
+
+#[derive(Debug)]
+pub struct Cooldown {
+    pub millis: u64,
+    pub micros: u64,
+    pub nanos: u64,
+}
+
+impl Default for Cooldown {
+    fn default() -> Self {
+        Self {
+            millis: 100,
+            micros: 0,
+            nanos: 0,
+        }
+    }
+}
+
+impl Cooldown {
+    pub fn as_duration(&self) -> Duration {
+        Duration::from_millis(self.millis)
+            + Duration::from_micros(self.micros)
+            + Duration::from_nanos(self.nanos)
+    }
 }
 
 pub struct SendBox<T>(pub T);
